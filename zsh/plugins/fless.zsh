09a53e26816d4319b7ff07e3f50dca240f4bbad3
Eric Torres <erictorres4@protonmail.com>
2020-06-22 10:50:33 -0700

    Remove zsh plugin files

--- zsh/plugins/fless.zsh
+++
@@ -1,7 +0,0 @@
-# Fuzzy-find a file and open it in less
-_run_fless() {
-    /usr/bin/fless && zle reset-prompt
-}
-
-zle -N _run_fless
-bindkey -M viins '^n' _run_fless
--- zsh/plugins/mkcd.zsh
+++
@@ -1,8 +0,0 @@
-# Make a directory, then change into it
-
-mkcd() {
-    [[ ! -d "${1}" ]] && mkdir --parents -- "${1}"
-    cd "${1}" || exit
-}
-
-autoload -Uz mkcd
